7a6ad3222e2d075d889bfb7c0aa69001a75e8b40
shivrampaudel <shivrampaudel@gmail.com>
2022-11-02 23:08:11 -0700

    cool beans

---
+++ mainscript.js
@@ -0,0 +1,10 @@
+function newStyle() {
+  let newColor = 'blue';
+  var elem = document.getElementById('title');
+  elem.style.color = newColor;
+}
+function oldStyle() {
+    let oldColor = "black";
+    var elem = document.getElementById('title');
+    elem.style.color = oldColor;
+}
