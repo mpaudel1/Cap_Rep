3f1fdb9c10cb49a31e7867d56fc0cc385e8d446e
shivrampaudel <shivrampaudel@gmail.com>
2022-11-22 11:31:31 -0700

    modified:   calc.html 	deleted:    calc_script.js 	modified:   calc_style.css 	deleted:    config.php     new file:   db.js 	modified:   home.html 	modified:   list.html 	modified:   list_script.js 	new file:   login.html 	renamed:    register_login_style.css -> login.js 	deleted:    login.php 	new file:   login_style.css 	new file:   register.html 	deleted:    register.php 	new file:   register_style.css 	deleted:    user_page.php

--- list_script.js
+++ list_script.js
@@ -1,3 +1,14 @@
 function budget(budget_subtraction) {
-    var product = document.getElementById('product');
+    var p1_cost = document.getElementById('p1_cost');
+    var p1_quantity = document.getElementById('p1_quantity');
+    var p1_rembalance = document.getElementById('p1_rembalance');
+    var budget = document.getElementById('budget')
+}
+function result() {
+
+    
+    if (!isNaN(numberValue))
+        console.log("The value=" + parseInt(dif));
+    else
+        console.log("Please enter the integer value..");
 }
